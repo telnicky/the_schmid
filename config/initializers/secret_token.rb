5327e8904f1ce94ad6e5dd03fbeb6c2de16fce6b
elnicky <travis.elnicky@gmail.com>
2013-02-21 18:23:16 -0700

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheScmid::Application.config.secret_token = '37247f4cf2bec5c28abf80c733cb035e5e86e68681e375d1b195cf62c45c76c2f09e9631eb9067c9ab1f0eb14d0db58d1ade4933eebf725029b4021f6cfc869b'
